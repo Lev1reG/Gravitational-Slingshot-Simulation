f4c1189dc2d857104aa40b0a9d02be7b6541d871
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-06-01 22:53:02 +0700

    feat/add pygame main loop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pygame
 import math
+
+import pygame
 
 pygame.init()
 
@@ -16,8 +17,30 @@ OBJ_SIZE = 5
 VEL_SCALE = 100
 
 BG = pygame.transform.scale(pygame.image.load("background.jpg"), (WIDTH, HEIGHT))
-PLANET = pygame.transform.scale(pygame.image.load("jupiter.jpg"), (PLANET_RADIUS * 2, PLANET_RADIUS * 2))
+PLANET = pygame.transform.scale(
+    pygame.image.load("jupiter.png"), (PLANET_RADIUS * 2, PLANET_RADIUS * 2)
+)
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+
+
+def main():
+    running = True
+    clock = pygame.time.Clock()
+
+    while running:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        win.blit(BG, (0, 0))
+        pygame.display.update()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
