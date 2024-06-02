f3c3718df09eb3fed69fc2142d6e70bc58bad4fd
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-06-02 16:37:58 +0700

    fix/laggy asteroids

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 import random as rd
+
 import pygame
 
 pygame.init()
@@ -11,9 +12,14 @@ pygame.display.set_caption("Gravitational Slingshot Effect Simulation")
 # Constants
 G = 6
 FPS = 60
-VEL_SCALE = 10
+VEL_SCALE = 100
+ASTEROID_RADIUS = rd.randint(10, 50)
 
 BG = pygame.transform.scale(pygame.image.load("background.jpg"), (WIDTH, HEIGHT))
+ASTEROID = pygame.transform.scale(
+    pygame.image.load("asteroid.png"), (ASTEROID_RADIUS, ASTEROID_RADIUS)
+)
+
 
 class Planet:
     def __init__(self, x, y, MASS, PLANET_NAME, PLANET_RADIUS):
@@ -22,19 +28,26 @@ class Planet:
         self.MASS = MASS
         self.PLANET_RADIUS = PLANET_RADIUS
         self.PLANET_NAME = PLANET_NAME
-        self.PLANET = pygame.transform.scale(pygame.image.load(self.PLANET_NAME + ".png"), (self.PLANET_RADIUS * 2, self.PLANET_RADIUS * 2))
-        self.MAX_ASTEROIDS = rd.randint(1, 5)
+        self.PLANET = pygame.transform.scale(
+            pygame.image.load(self.PLANET_NAME + ".png"),
+            (self.PLANET_RADIUS * 2, self.PLANET_RADIUS * 2),
+        )
+        self.MAX_ASTEROIDS = rd.randint(1, 10)
 
     def draw(self):
-        win.blit(self.PLANET, (self.x - self.PLANET_RADIUS, self.y - self.PLANET_RADIUS))
+        win.blit(
+            self.PLANET, (self.x - self.PLANET_RADIUS, self.y - self.PLANET_RADIUS)
+        )
 
     def simulate(self, MAX_ASTEROID_MASS, MIN_ASTEROID_VEL, MAX_ASTEROID_VEL):
         running = True
         clock = pygame.time.Clock()
 
-        planet = Planet(WIDTH // 2, HEIGHT // 2, self.MASS, self.PLANET_NAME, self.PLANET_RADIUS) 
+        planet = Planet(
+            WIDTH // 2, HEIGHT // 2, self.MASS, self.PLANET_NAME, self.PLANET_RADIUS
+        )
         objects = []
-        done_adding_asteroids = False 
+        done_adding_asteroids = False
 
         while running:
             clock.tick(FPS)
@@ -51,12 +64,15 @@ class Planet:
                     done_adding_asteroids = True
 
             win.blit(BG, (0, 0))
-            
+
             for obj in objects[:]:
                 obj.draw()
                 obj.move(planet)
                 off_screen = obj.x < 0 or obj.x > WIDTH or obj.y < 0 or obj.y > HEIGHT
-                collided = math.sqrt((obj.x - planet.x)**2 + (obj.y - planet.y)**2) <= self.PLANET_RADIUS
+                collided = (
+                    math.sqrt((obj.x - planet.x) ** 2 + (obj.y - planet.y) ** 2)
+                    <= self.PLANET_RADIUS
+                )
                 if off_screen or collided:
                     objects.remove(obj)
 
@@ -66,12 +82,12 @@ class Planet:
 
         pygame.quit()
 
+
 class Asteroid:
     def __init__(self, MAX_MASS):
         self.x = rd.randint(0, WIDTH)
         self.y = rd.randint(0, HEIGHT)
         self.MASS = rd.randint(1, MAX_MASS)
-        self.ASTEROID_RADIUS = rd.randint(10, 50)
 
     def set_velocity(self, min_vel, max_vel):
         self.vel_x = rd.randint(min_vel, max_vel)
@@ -80,7 +96,7 @@ class Asteroid:
         self.vel_y /= VEL_SCALE
 
     def move(self, planet):
-        distance = math.sqrt((self.x - planet.x)**2 + (self.y - planet.y)**2)
+        distance = math.sqrt((self.x - planet.x) ** 2 + (self.y - planet.y) ** 2)
 
         force = (G * self.MASS * planet.MASS) / distance**2
         acceleration = force / self.MASS
@@ -96,22 +112,22 @@ class Asteroid:
         self.y += self.vel_y
 
     def draw(self):
-        ASTEROID = pygame.transform.scale(
-            pygame.image.load("asteroid.png"), (self.ASTEROID_RADIUS, self.ASTEROID_RADIUS)
-        )
         win.blit(ASTEROID, (int(self.x), int(self.y)))
+
 
 def jupiter():
     MAX_ASTEROID_MASS = 6
-    MIN_ASTEROID_VEL = -10
-    MAX_ASTEROID_VEL = 10
-    PLANET_MASS = 10000
+    MIN_ASTEROID_VEL = -100
+    MAX_ASTEROID_VEL = 100
+    PLANET_MASS = 100
     PLANET_RADIUS = 200
     planet = Planet(WIDTH // 2, HEIGHT // 2, PLANET_MASS, "jupiter", PLANET_RADIUS)
     planet.simulate(MAX_ASTEROID_MASS, MIN_ASTEROID_VEL, MAX_ASTEROID_VEL)
-    
+
+
 def main():
-   jupiter() 
+    jupiter()
+
 
 if __name__ == "__main__":
     main()
