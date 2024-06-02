12ea268b4978e685862b8ad9ca01efd0c61dc34e
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-06-02 16:22:40 +0700

    feat/randomize asteroids vel and position

--- main.py
+++ main.py
@@ -4,23 +4,16 @@ import pygame
 
 pygame.init()
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1920, 1200
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Gravitational Slingshot Effect Simulation")
 
 # Constants
-PLANET_MASS = 100
-ASTEROID_MASS = 5
-G = 5
+G = 6
 FPS = 60
-PLANET_RADIUS = 60
-ASTEROID_RADIUS = 35
-VEL_SCALE = 100
+VEL_SCALE = 10
 
 BG = pygame.transform.scale(pygame.image.load("background.jpg"), (WIDTH, HEIGHT))
-ASTEROID = pygame.transform.scale(
-    pygame.image.load("asteroid.png"), (ASTEROID_RADIUS, ASTEROID_RADIUS)
-)
 
 class Planet:
     def __init__(self, x, y, MASS, PLANET_NAME, PLANET_RADIUS):
@@ -30,46 +23,40 @@ class Planet:
         self.PLANET_RADIUS = PLANET_RADIUS
         self.PLANET_NAME = PLANET_NAME
         self.PLANET = pygame.transform.scale(pygame.image.load(self.PLANET_NAME + ".png"), (self.PLANET_RADIUS * 2, self.PLANET_RADIUS * 2))
+        self.MAX_ASTEROIDS = rd.randint(1, 5)
 
     def draw(self):
         win.blit(self.PLANET, (self.x - self.PLANET_RADIUS, self.y - self.PLANET_RADIUS))
 
-    def simulate(self):
+    def simulate(self, MAX_ASTEROID_MASS, MIN_ASTEROID_VEL, MAX_ASTEROID_VEL):
         running = True
         clock = pygame.time.Clock()
 
         planet = Planet(WIDTH // 2, HEIGHT // 2, self.MASS, self.PLANET_NAME, self.PLANET_RADIUS) 
         objects = []
-        temp_obj_pos = None
+        done_adding_asteroids = False 
 
         while running:
             clock.tick(FPS)
 
-            mouse_pos = pygame.mouse.get_pos()  # get the mouse position (x, y)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-                # if the mouse is clicked
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if temp_obj_pos:
-                        obj = create_asteroid(temp_obj_pos, mouse_pos)
+                while not done_adding_asteroids:
+                    for i in range(1, self.MAX_ASTEROIDS):
+                        obj = Asteroid(MAX_ASTEROID_MASS)
+                        obj.set_velocity(MIN_ASTEROID_VEL, MAX_ASTEROID_VEL)
                         objects.append(obj)
-                        temp_obj_pos = None
-                    else:
-                        temp_obj_pos = mouse_pos
+                    done_adding_asteroids = True
 
             win.blit(BG, (0, 0))
-
-            if temp_obj_pos:
-                pygame.draw.line(win, (255, 255, 255), temp_obj_pos, mouse_pos, 2)
-                win.blit(ASTEROID, (temp_obj_pos[0], temp_obj_pos[1]))
-
+            
             for obj in objects[:]:
                 obj.draw()
                 obj.move(planet)
                 off_screen = obj.x < 0 or obj.x > WIDTH or obj.y < 0 or obj.y > HEIGHT
-                collided = math.sqrt((obj.x - planet.x)**2 + (obj.y - planet.y)**2) <= PLANET_RADIUS
+                collided = math.sqrt((obj.x - planet.x)**2 + (obj.y - planet.y)**2) <= self.PLANET_RADIUS
                 if off_screen or collided:
                     objects.remove(obj)
 
@@ -80,18 +67,23 @@ class Planet:
         pygame.quit()
 
 class Asteroid:
-    def __init__(self, x, y, vel_x, vel_y, mass):
-        self.x = x
-        self.y = y
-        self.vel_x = vel_x
-        self.vel_y = vel_y
-        self.mass = mass
+    def __init__(self, MAX_MASS):
+        self.x = rd.randint(0, WIDTH)
+        self.y = rd.randint(0, HEIGHT)
+        self.MASS = rd.randint(1, MAX_MASS)
+        self.ASTEROID_RADIUS = rd.randint(10, 50)
+
+    def set_velocity(self, min_vel, max_vel):
+        self.vel_x = rd.randint(min_vel, max_vel)
+        self.vel_x /= VEL_SCALE
+        self.vel_y = rd.randint(min_vel, max_vel)
+        self.vel_y /= VEL_SCALE
 
     def move(self, planet):
         distance = math.sqrt((self.x - planet.x)**2 + (self.y - planet.y)**2)
 
-        force = (G * self.mass * planet.MASS) / distance**2
-        acceleration = force / self.mass
+        force = (G * self.MASS * planet.MASS) / distance**2
+        acceleration = force / self.MASS
         angle = math.atan2(planet.y - self.y, planet.x - self.x)
 
         acceleration_x = acceleration * math.cos(angle)
@@ -102,22 +94,24 @@ class Asteroid:
 
         self.x += self.vel_x
         self.y += self.vel_y
-        
 
     def draw(self):
+        ASTEROID = pygame.transform.scale(
+            pygame.image.load("asteroid.png"), (self.ASTEROID_RADIUS, self.ASTEROID_RADIUS)
+        )
         win.blit(ASTEROID, (int(self.x), int(self.y)))
 
-def create_asteroid(location, mouse):
-    t_x, t_y = location
-    m_x, m_y = mouse
-    vel_x = (m_x - t_x) / VEL_SCALE
-    vel_y = (m_y - t_y) / VEL_SCALE
-    obj = Asteroid(t_x, t_y, vel_x, vel_y, ASTEROID_MASS) 
-    return obj
-
-def main():
+def jupiter():
+    MAX_ASTEROID_MASS = 6
+    MIN_ASTEROID_VEL = -10
+    MAX_ASTEROID_VEL = 10
+    PLANET_MASS = 10000
+    PLANET_RADIUS = 200
     planet = Planet(WIDTH // 2, HEIGHT // 2, PLANET_MASS, "jupiter", PLANET_RADIUS)
-    planet.simulate()
+    planet.simulate(MAX_ASTEROID_MASS, MIN_ASTEROID_VEL, MAX_ASTEROID_VEL)
+    
+def main():
+   jupiter() 
 
 if __name__ == "__main__":
     main()
